e9afc3981c1432a40e470b57d6a7dd1340c94b87
atillacay <atillacay@gmail.com>
2021-11-06 00:50:31 +0300

    init commit, axios, bulma installed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bulma/css/bulma.min.css';
+
+function App() {
+  return (
+    <div className="container">
+
+      <h1>Weather App</h1>
+
+    </div>
+  );
+}
+
+export default App;
